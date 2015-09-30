47a82eb1114db026e298db287956c956508d6ad0
Harriet Craven <harrietc52@hotmail.co.uk>
2015-09-30 11:24:55 +0100

    added down temp button

--- src/Application.js
+++ src/Application.js
@@ -2,8 +2,13 @@ $( document ).ready (function() {
 
   $('#temp').text(thermostat.temperature);
 
-  $('#upTemp').click(function(event) {
+  $('#upTemp').click(function() {
     thermostat.upTemp();
+    $('#temp').text(thermostat.temperature);
+  });
+
+  $('#downTemp').click(function() {
+    thermostat.downTemp();
     $('#temp').text(thermostat.temperature);
   });
 
