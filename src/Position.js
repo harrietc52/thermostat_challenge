a32d6c6b048358521ed28932f14b5429dcd1a59f
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-02 15:25:21 +0100

    lat and lon working

--- src/Position.js
+++ src/Position.js
@@ -11,4 +11,25 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude.toFixed(2));
 };
 
+function getWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(weatherRequest);
+    } else {
+        $("#position").append("Geolocation is not supported by this browser.");
+    };
+};
+
+function weatherRequest(position) {
+  var lat = position.coords.latitude.toFixed(2)
+  var lon = position.coords.longitude.toFixed(2)
+   $.ajax({
+   url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '',
+   success: function(data){
+     console.log(data)
+     $('#weather').html('Current temperature in ' + data.name + ': ' + (data.main.temp - 273.15).toFixed(1))
+   }
+  });
+ };
+
 getLocation();
+getWeather();
